7a7c0fb5eeeaf3bce320fabca1e809992c047dbf
lorenzo-lettieri <Matteo07>
2021-12-13 12:16:23 +0100

    cleanup and upgrade

--- src/main/java/ch/bbw/zork/Zork2.java
+++ src/main/java/ch/bbw/zork/Zork2.java
@@ -1,13 +1,8 @@
 package ch.bbw.zork;
 
 public class Zork2 {
-
-	public static void main(String[] args) {
-		Game zorkgame = new Game();
-		zorkgame.play();
-	}
-
+    public static void main(String[] args) {
+        Game zorkgame = new Game();
+        zorkgame.play();
+    }
 }
-
-
-
